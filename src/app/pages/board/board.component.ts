2308ebbdbaa72edabafc8f9c63c2a4e66b62af29
gerardogarnica <gerardo.garnica@gmail.com>
2023-03-16 22:34:51 -0500

    Add icons and sidebar menu.

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { faBox, faWaveSquare, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html'
 })
 export class BoardComponent {
+  faBox = faBox;
+  faWaveSquare = faWaveSquare;
+  faClock = faClock;
+  faTrello = faTrello;
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
         danger: colors.red,
         primary: colors.blue,
         success: colors.green
+      },
+      container: {
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1024px',
+          '2xl': '1280px',
+        },
       }
     },
   },
