125156720304a628f9d2bad636be5a2d67219d2d
gerardogarnica <gerardo.garnica@gmail.com>
2023-03-22 20:57:58 -0500

    Applying best practices to components.

--- src/app/components/main-button/main-button.component.ts
+++ src/app/components/main-button/main-button.component.ts
@@ -9,22 +9,40 @@ export class MainButtonComponent {
   @Input() type: 'button' | 'submit' | 'reset' = 'button';
   @Input() colorStyle: 'danger' | 'primary' | 'shadow' | 'success' = 'primary';
 
+  mapColors = {
+    success: {
+      'text-white': true,
+      'bg-success-600': true,
+      'hover:bg-success-800': true,
+      'focus:ring-success-300': true
+    },
+    danger: {
+      'text-white': true,
+      'bg-danger-600': true,
+      'hover:bg-danger-800': true,
+      'focus:ring-danger-300': true
+    },
+    primary: {
+      'text-white': true,
+      'bg-primary-600': true,
+      'hover:bg-primary-800': true,
+      'focus:ring-primary-300': true
+    },
+    shadow: {
+      'text-black': true,
+      'bg-gray-200': true,
+      'hover:bg-gray-500': true,
+      'focus:ring-gray-50': true
+    }
+  }
+
   get colors(): any {
-    return {
-      'text-white': this.colorStyle === 'danger' || this.colorStyle === 'primary' || this.colorStyle === 'success',
-      'text-black': this.colorStyle === 'shadow',
-      'bg-danger-600': this.colorStyle === 'danger',
-      'hover:bg-danger-800': this.colorStyle === 'danger',
-      'focus:ring-danger-300': this.colorStyle === 'danger',
-      'bg-primary-600': this.colorStyle === 'primary',
-      'hover:bg-primary-800': this.colorStyle === 'primary',
-      'focus:ring-primary-300': this.colorStyle === 'primary',
-      'bg-gray-200': this.colorStyle === 'shadow',
-      'hover:bg-gray-500': this.colorStyle === 'shadow',
-      'focus:ring-gray-50': this.colorStyle === 'shadow',
-      'bg-success-600': this.colorStyle === 'success',
-      'hover:bg-success-800': this.colorStyle === 'success',
-      'focus:ring-success-300': this.colorStyle === 'success'
-    };
+    const colors = this.mapColors[this.colorStyle];
+
+    if (colors) {
+      return colors;
+    }
+
+    return {};
   }
 }
