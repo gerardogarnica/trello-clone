9e23aebe0a239ee4c6e7fe214ad917e80f46fbba
gerardogarnica <gerardo.garnica@gmail.com>
2023-03-25 17:14:51 -0500

    Add auth and main modules.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// Pages
-import { BoardComponent } from './pages/board/board.component';
-import { BoardItemComponent } from './pages/board-item/board-item.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ScrollComponent } from './pages/scroll/scroll.component';
-import { TableComponent } from './pages/table/table.component';
-
 const routes: Routes = [
   {
-    path: 'login',
-    component: LoginComponent
+    path: '',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
   },
   {
-    path: 'boards',
-    component: BoardComponent
-  },
-  {
-    path: 'board',
-    component: BoardItemComponent
-  },
-  {
-    path: 'scroll',
-    component: ScrollComponent
-  },
-  {
-    path: 'table',
-    component: TableComponent
+    path: 'trello',
+    loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)
   }
 ];
 
