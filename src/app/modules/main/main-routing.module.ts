220d8feeb1bb48ae3a913b01cad24adcf20a6ba6
gerardogarnica <gerardo.garnica@gmail.com>
2023-03-25 22:25:10 -0500

    Add boards module.

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './components/layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'boards',
+        pathMatch: 'full'
+      },
+      {
+        path: 'boards',
+        loadChildren: () => import('../boards/boards.module').then(m => m.BoardsModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+// Routing
 import { MainRoutingModule } from './main-routing.module';
 
+// Components
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+// Pages
+
+// Modules
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+    NavBarComponent
+  ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    OverlayModule,
+    FontAwesomeModule,
+    SharedModule
   ]
 })
 export class MainModule { }
